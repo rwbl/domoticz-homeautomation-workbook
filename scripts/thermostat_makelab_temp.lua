a4eecde8e12fcb67687a6724341f3d5fdbe4d2d9
Robert W.B. Linn <rwblinn@outlook.de>
2019-12-07 18:35:10 +0100

    Update thermostat_makelab_temp.lua

--- scripts/thermostat_makelab_temp.lua
+++ scripts/thermostat_makelab_temp.lua
@@ -1,7 +1,7 @@
 -- thermostat_makelab_temp
 -- Get information from the makelab homematicIP thermostat
 -- HmIP-eTRV-2 000A18A9A64DAC:1
--- Obtain the device information and extract the temperature from the response
+-- Request datapoint information and extract the value (temperature) from the response
 -- http://192.168.1.225/config/xmlapi/state.cgi?datapoint_id=1567
 -- HTTP response showing the datapoints 1567 (temperature)
 -- <?xml version="1.0" encoding="ISO-8859-1"?>
@@ -9,9 +9,10 @@
 --  <datapoint value="22.400000" ise_id="1567"/>
 -- </state>
 
--- Domoticz device text (named MakeLab Thermostat Info)
+-- Domoticz device
 local IDX_MAKELAB_THERMOSTAT_TEMP = 65;
 
+-- homematic device datapoint id
 local ID_DATAPOINT = 1567;
 
 -- url of the raspmatic webserver to obtain device information
@@ -41,22 +42,22 @@ return {
 	execute = function(domoticz, item)
 		-- domoticz.log('Device ' .. domoticz.devices(IDX_MAKELAB_THERMOSTAT_INFO).name .. ' was changed to ' .. domoticz.devices(IDX_MAKELAB_THERMOSTAT_INFO).state, domoticz.LOG_INFO)
 
-        -- check if the item is a device, then request information
+        	-- check if the item is a device, then request information
 		if (item.isTimer) then
 			domoticz.openURL({url = URL_RASPMATIC, method = 'GET', callback = RES_RASPMATIC})
 		end
 
-        -- check if the item is a httpresponse from the openurl callback
+        	-- check if the item is a http response from the openurl callback
 		if (item.isHTTPResponse) then
 			if (item.statusCode == 200) then
 
-                -- TEMPERATURE
-                -- <datapoint value="22.400000" ise_id="1567"/>
-                local temperaturevalue = domoticz_applyXPath(item.data,'//datapoint[@ise_id="1567"]/@value')  -- C
+                		-- TEMPERATURE
+                		-- <datapoint value="22.400000" ise_id="1567"/>
+                		local temperaturevalue = domoticz_applyXPath(item.data,'//datapoint[@ise_id="1567"]/@value')  -- C
                 
-                -- log
-                local msg = 'Temperature: ' .. tostring(round(temperaturevalue,DECIMALS))
-                domoticz.log(msg) 
+                		-- log
+                		local msg = 'Temperature: ' .. tostring(round(temperaturevalue,DECIMALS))
+                		domoticz.log(msg) 
 				
 				-- update the domoticz device
 				domoticz.devices(IDX_MAKELAB_THERMOSTAT_TEMP).updateTemperature(round(temperaturevalue,1));
